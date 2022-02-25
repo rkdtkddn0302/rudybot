f9c4760937c7ee9c2d30759dad38583ee69a3d8e
rkdtkddn0302 <100404127+rkdtkddn0302@users.noreply.github.com>
2022-02-25 22:29:51 +0900

    .
    .

--- rudybot.py
+++ rudybot.py
@@ -2,16 +2,12 @@ import discord
 from discord.ext import commands
 import os
 import random
-from discord.activity import game
 from random import *
 client = commands.Bot(command_prefix = '-')
 
 @client.event
 async def on_ready():
-
-  print(client.user.name)
-  game = discord.game('루디는 열심히 일')
-  await client.change_presence(stauts=discord.Status.online, activity=game)
+  await client.change_presence(status=discord.Status.online)
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 @client.event
