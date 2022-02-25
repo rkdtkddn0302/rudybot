fe4f6d3b101328e73c14058785210c3114cd1f50
rkdtkddn0302 <100404127+rkdtkddn0302@users.noreply.github.com>
2022-02-25 22:27:03 +0900

    .
    .

--- rudybot.py
+++ rudybot.py
@@ -1,21 +1,22 @@
 import discord
 from discord.ext import commands
 import os
-
+import random
+from discord.activity import game
+from random import *
 client = commands.Bot(command_prefix = '-')
 
 @client.event
 async def on_ready():
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
-
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
+  print(client.user.name)
+  game = discord.game('루디는 열심히 일')
+  await client.change_presence(stauts=discord.Status.online, activity=game)
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
+@client.event
+async def on_message(message):
+  if message.content == "안녕":
+    await message.channel.send("하이")
 
 client.run(os.environ['token'])
